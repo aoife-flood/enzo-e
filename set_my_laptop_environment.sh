b5bebe05166c7ef48f90c485f437259066d426e0
Stefan Arridge <stefan.arridge@durham.ac.uk>
2020-10-07 14:32:30 +0100

    Adds set_my_laptop_environment.sh which sets environment variables necessary to compile Enzo-E

---
+++ set_my_laptop_environment.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Setting environment variables"
+export CHARM_HOME=/home/pdjn36/Codes/charm
+export CELLO_ARCH=linux_gnu
+export CELLO_PREC=double
+echo "CHARM_HOME = $CHARM_HOME"
+echo "CELLO_ARCH = $CELLO_ARCH"
+echo "CELLO_PREC = $CELLO_PREC"
